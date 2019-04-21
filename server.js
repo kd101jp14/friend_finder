c9d8540ca5dc33adbc845001f3b1227eaf38c47e
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-20 20:58:18 -0500

    Updated server.

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Variables for Express app configuration
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middlewear to set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Listener on port 3000
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
