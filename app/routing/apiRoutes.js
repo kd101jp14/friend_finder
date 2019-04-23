471eb84359942474d8467dded06045b7d413cdaa
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-23 02:13:30 -0500

    The '/api/friends' route successfully displays friends in json format.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,26 +3,15 @@
 // * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 // * A POST routes `/api/friends`. This will be used to handle incoming survey results.
 //   This route will also be used to handle the compatibility logic.
+
+var friends = require("../data/friends.js");
+
 module.exports = function(app) {
 // Display all possible friends
 app.get("/api/friends", function(req, res) {
   return res.json(friends);
 });
 
-// Displays a single friend, or returns false
-app.get("/api/friends/:friend", function(req, res) {
-  var chosen = req.params.friend;
-
-  console.log(chosen);
-
-  for (var i = 0; i < friends.length; i++) {
-    if (chosen === friends[i].routeName) {
-      return res.json(friends[i]);
-    }
-  }
-
-  return res.json(false);
-});
 
 // * Convert each user's results into a simple array of numbers 
 // (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
