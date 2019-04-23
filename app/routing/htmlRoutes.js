378396b9522d192da96486d7377b0839fe6d83c1
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-23 04:26:58 -0500

    Made a few minor updates on files before adding logic for showing friend.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,6 @@
-// 3. Your `htmlRoutes.js` file should include two routes:
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default, catch-all route that leads to `home.html` which displays the home page.
 var path = require("path");
 
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
 
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
